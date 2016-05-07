26c3fd83e485949a807142a4ea5e0d617dc92237
Ian Philpot <ian.philpot@microsoft.com>
2016-05-07 09:16:48 -0400

    sec stuff

--- DeployRHEL.py
+++ DeployRHEL.py
@@ -16,14 +16,23 @@ from azure.mgmt.resource.resources.models import DeploymentProperties
 from azure.mgmt.resource.resources.models import DeploymentMode
 from azure.mgmt.resource.resources.models import ParametersLink
 from azure.mgmt.resource.resources.models import TemplateLink
-from pprint import pprint
+# from pprint import pprint
 
 
 subscription_id = '65e7482b-addd-4edf-8d8f-dbfefc56f2e0'
 
+userFile = "user.pw"
+passFile = "pass.pw"
+
+userTxt = open(userFile)
+passTxt = open(passFile)
+
+username = userTxt.read()
+password = passTxt.read()
+
 credentials = UserPassCredentials(
-    'ian@ianphilpot.onmicrosoft.com',    # Your new user
-    'Aw3someP@ss',  # Your password
+    username,    # Your new user
+    password,  # Your password
 )
 
 resource_client = ResourceManagementClient(
@@ -46,27 +55,3 @@ result = resource_client.resource_groups.create_or_update(
 
 print "Created Resource Group:", group_name
     
-deployment_name = 'SyntaxConDep'
-
-template = TemplateLink(
-    uri='https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-rhel/azuredeploy.json',
-)
-
-parameters = ParametersLink(
-    uri='https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-rhel/azuredeploy.parameters.json',
-)
-
-result = resource_client.deployments.create_or_update(
-    group_name,
-    deployment_name,
-    properties=DeploymentProperties(
-        mode=DeploymentMode.incremental,
-        template_link=template,
-        parameters_link=parameters,
-    )
-)
-
-print "Resource Deployment: There are %s exceptions" % result._exception
-print "Created Deployment:", deployment_name
-
-# pprint (vars(result))
